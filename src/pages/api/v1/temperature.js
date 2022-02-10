24912dbdc9444cc1bd64964c3f0e73eb14066d88
MajedFakhrEldin <majed.ctb@gmail.com>
2022-02-10 18:03:26 +0100

    First scetch for temperature.js

---
+++ src/pages/api/v1/temperature.js
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+//
+export default function handler(req, res) {
+    res.status(200).json({ status: 'Success', timestamp: "2022-02-10:17:56:53", UTC_offset: 1, id: 1, temp: 16, unit: "C"})
+  }
+  
