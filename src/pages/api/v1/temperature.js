965907bb8459207a03f2be72d4cd39e1b8a95d0c
MajedFakhrEldin <majed.ctb@gmail.com>
2022-02-13 17:48:52 +0100

    added a comment

--- src/pages/api/v1/temperature.js
+++ src/pages/api/v1/temperature.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-//
+//Example of a row in a database
 export default function handler(req, res) {
     res.status(200).json({ status: 'Success', timestamp: "2022-02-10:17:56:53", UTC_offset: 1, id: 1, temp: 16, unit: "C"})
   }
