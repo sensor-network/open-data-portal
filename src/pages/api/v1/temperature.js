65248f07942de3c20ce08d842391efe0fd05177a
MajedFakhrEldin <majed.ctb@gmail.com>
2022-02-17 12:04:56 +0100

    deleted comment

--- src/pages/api/v1/temperature.js
+++ src/pages/api/v1/temperature.js
@@ -40,8 +40,4 @@ export default async function handler(req, res){
 }
 
 
-//Example of a row in a database
-/*export default function handler(req, res) {
-    res.status(200).json({ status: 'Success', timestamp: "2022-02-10:17:56:53", UTC_offset: 1, id: 1, temp: 16, unit: "C"})
-  }*/
   
