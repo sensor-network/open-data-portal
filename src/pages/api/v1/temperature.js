4d123031c5c658ea5756dd2bc50f270fda49742f
MajedFakhrEldin <majed.ctb@gmail.com>
2022-02-19 16:01:26 +0100

    temperature endpoint returns an array instead of object

--- src/pages/api/v1/temperature.js
+++ src/pages/api/v1/temperature.js
@@ -40,7 +40,7 @@ export default async function handler(req, res){
       }
     }
     
-    res.status(200).json({ content: data});
+    res.status(200).json(data);
   }
   catch(e) {
     console.error(e);
