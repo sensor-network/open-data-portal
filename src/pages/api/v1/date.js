2d6a89f691a24ca390068511c26103ec39544d7d
MajedFakhrEldin <majed.ctb@gmail.com>
2022-02-21 17:01:22 +0100

    Added status variables

--- src/pages/api/v1/date.js
+++ src/pages/api/v1/date.js
@@ -3,12 +3,17 @@
 import mysql from "mysql2/promise"
 import moment from "moment"
 
+const STATUS_OK = 200
+const STATUS_BAD_REQUEST = 400
+const STATUS_METHOD_NOT_ALLOWED = 405
+const STATUS_SERVER_ERROR = 500
+
 export default async function handler(req, res){
   
   // Only allow GET-requests for this endpoint. I do not know how to test this
   if (req.method !== "GET") {
     console.log(`Error: Method ${req.method} not allowed.`)
-    res.status(405)        // 405: method not allowed
+    res.status(STATUS_METHOD_NOT_ALLOWED)        // 405: method not allowed
         .json({ error:
             `Method ${req.method} is not allowed for this endpoint. Please read the documentation on how to query the endpoint.`
     });
@@ -21,11 +26,11 @@ export default async function handler(req, res){
     const endDate = moment(req.query.endDate, 'YYYY-MM-DD', true);
 
     if(startDate.isValid() !== true || endDate.isValid() !== true){
-      res.status(400).json({error: "Invalid parameters. Please, read the documentation for valid parameters"})
+      res.status(STATUS_BAD_REQUEST).json({error: "Invalid parameters. Please, read the documentation for valid parameters"})
       return;
     }
     if(startDate.isAfter(endDate)){
-      res.status(400).json({error: "Start date can't be after End date!"})
+      res.status(STATUS_BAD_REQUEST).json({error: "Start date can't be after End date!"})
       return;
     }
     //Connecting to the database
@@ -50,10 +55,10 @@ export default async function handler(req, res){
     connection.destroy();
     
     
-    res.status(200).json(data);
+    res.status(STATUS_OK).json(data);
   }
   catch(e) {
     console.error(e);
-    res.status(500).json({ error: "Error fetching data from the database" });
+    res.status(STATUS_SERVER_ERROR).json({ error: "Error fetching data from the database" });
   }
 }
