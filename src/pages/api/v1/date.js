7d7a103e37edb6a4151741b53e51b6b12817d13d
MajedFakhrEldin <majed.ctb@gmail.com>
2022-02-22 16:41:13 +0100

    changed to connection.end()

--- src/pages/api/v1/date.js
+++ src/pages/api/v1/date.js
@@ -55,7 +55,7 @@ export default async function handler(req, res){
     const [data] = await connection.execute(query);
     
     //Disconnecting from the database
-    connection.destroy();
+    await connection.end();
     
     
     res.status(STATUS_OK).json(data);
