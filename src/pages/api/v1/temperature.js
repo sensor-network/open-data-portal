70846b62e28ec01943e897c5cbec8e50ec62bc19
Julius <julius0216@outlook.com>
2022-03-05 17:57:56 +0100

    updated API Endpoint `/temperature` to use `Temperature`-unit-system

--- src/pages/api/v1/temperature.js
+++ src/pages/api/v1/temperature.js
@@ -1,11 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 //This file will be responsible for querying only the the temperature and the date columns.
 import mysql from "mysql2/promise"
+import { ZodError } from "zod";
 
 import { getConnectionPool } from "src/lib/database";
-
-import { temperatureFromKelvin } from "../../../lib/conversions/convertTemperature.js"
-import { ConversionError } from "../../../lib/CustomErrors";
 
 import {
     STATUS_OK,
@@ -13,6 +11,7 @@ import {
     STATUS_METHOD_NOT_ALLOWED,
     STATUS_SERVER_ERROR
 } from "src/lib/httpStatusCodes";
+import { parseUnit } from "src/lib/units/temperature";
 
 export default async function handler(req, res){
     // Only allow GET-requests for this endpoint. I do not know how to test this
@@ -36,19 +35,18 @@ export default async function handler(req, res){
         //Executing the query
         const [data] = await connection.query(query);
 
-        let unit = req.query.unit || 'K';   // fallback to `Kelvin` if not specified
-        if(unit.toUpperCase() !== 'K'){
-            for(const obj of data){
-                obj.temperature = temperatureFromKelvin(obj.temperature, unit);
-            }
+        const unit = parseUnit(req.query.unit || 'k');
+        for (const row of data) {
+            row.temperature = unit.fromKelvin(row.temperature);
         }
 
         res.status(STATUS_OK).json(data);
     }
     catch(e) {
-        if (e instanceof ConversionError) {     // Errors from converting
-            console.error(e);
-            res.status(STATUS_BAD_REQUEST).json({ error: e.message });
+        if (e instanceof ZodError) {     // Errors from parsing unit and converting
+            console.log("ERROR: Could not parse query parameters:\n", e.flatten())
+            res.status(STATUS_BAD_REQUEST)
+                .json(e.flatten());
         }
         else {      // Other unknown errors
             console.error(e);
