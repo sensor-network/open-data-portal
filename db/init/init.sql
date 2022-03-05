a5e91d146d39119f2db03193d7e4b5a1655a3d9f
nilsperssonsuorra <82161471+nilsperssonsuorra@users.noreply.github.com>
2022-03-05 15:10:49 +0100

    Locations table and endpoint
    Added a locations table to the database and an endpoint where you get all locations.

--- db/init/init.sql
+++ db/init/init.sql
@@ -8,4 +8,13 @@ CREATE TABLE `Data` (
   PRIMARY KEY (`id`),
   SPATIAL KEY `position` (`position`),
   CONSTRAINT `Data_chk_1` CHECK ((`pH` between 0 and 14))
+);
+
+CREATE TABLE `Locations` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `radius` int unsigned NOT NULL,
+  `position` point NOT NULL /*!80003 SRID 4326 */,
+  PRIMARY KEY (`id`),
+  SPATIAL KEY `position` (`position`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
