55bfb0d3e498d16e4f0f94d69d27108ad54dd472
MajedFakhrEldin <majed.ctb@gmail.com>
2022-03-06 23:25:09 +0100

    Began adding the export for the data.

---
+++ src/pages/data.js
@@ -0,0 +1,14 @@
+
+function App(){
+    function getData(){
+
+    }
+    return(
+        <div>
+            <button type="button" onClick={getData}>Get data</button>
+            <p id="allData"></p>
+        </div>
+    )
+}
+
+export default App;
