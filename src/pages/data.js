2f2d8e35b92aa4b91827e6ab7b7816c163b9f885
MajedFakhrEldin <majed.ctb@gmail.com>
2022-03-09 20:31:38 +0100

    Made getServerSideProps function

--- src/pages/data.js
+++ src/pages/data.js
@@ -1,14 +1,35 @@
+import useSWR from 'swr'
 
-function App(){
-    function getData(){
+const fetcher = (...args) => fetch(...args).then(res => res.json());
 
+export async function getServerSideProps(){
+    const endpointUrl = "http://localhost:3000/api/v1";
+    const data = await fetcher(endpointUrl);
+    return{
+        props: {
+            initialData: data
+        }
     }
-    return(
-        <div>
-            <button type="button" onClick={getData}>Get data</button>
-            <p id="allData"></p>
+}
+
+function App(props){
+    console.log(props)
+    const initialData = props.initialData;
+    
+
+    const endpointUrl = "http://localhost:3000/api/v1";
+    const { data, error } = useSWR(endpointUrl, fetcher);
+
+    if (error) return <div>failed to load</div>;
+    if (!data) return <div>loading...</div>;
+    
+  /*  return <div>{data.map( (row) => (
+        <div key= {row.id}>
+          {row.id}
+          {row.temperature}
         </div>
-    )
+      ))}</div>;*/
+      return <p>idk</p>
 }
 
 export default App;
