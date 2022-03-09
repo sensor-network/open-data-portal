9d686aa1db99e056df29cccea7dea936a8e1606b
MajedFakhrEldin <majed.ctb@gmail.com>
2022-03-10 00:44:47 +0100

    Returned the data fetched by the API

--- src/pages/data.js
+++ src/pages/data.js
@@ -24,12 +24,12 @@ function App(props){
     if (error) return <div>failed to load</div>;
     if (!data) return <div>loading...</div>;
     
-  /*  return <div>{data.map( (row) => (
-        <div key= {row.id}>
-          {row.id}
-          {row.temperature}
-        </div>
-      ))}</div>;*/
+    return <div>
+        {data.map( (row) => (
+            <div key= {row.id}>
+                {row.id} {row.pH} {row.temperature} {row.conductivity} {row.date} {row.longitude} {row.latitude}
+            </div>
+      ))}</div>;
 }
 
 export default App;
