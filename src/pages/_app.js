e6a70787022dd18e7cbb70fd7f97dc87c929a26b
Julius <julius0216@outlook.com>
2022-03-10 20:38:43 +0100

    refactor preferences to useContext

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import 'src/styles/globals.css';
+import NextNProgress from 'nextjs-progressbar';
+import { createContext, useState } from 'react';
+import Cookies from 'js-cookie';
+
+import Navbar from 'src/components/Navbar';
+import { loadPreferences } from 'src/lib/loadPreferences';
+
+export const PreferenceContext = createContext();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const [preferences, setPreferences] = useState(() => (
+        loadPreferences(Cookies.get('preferences'))
+    ));
+    return (
+        <>
+            <NextNProgress color={"#1976d2"} height={5} options={{ showSpinner: false }} />
+            <PreferenceContext.Provider value={preferences}>
+                <Navbar setPreferences={setPreferences}/>
+                <Component {...pageProps} />
+            </PreferenceContext.Provider>
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
