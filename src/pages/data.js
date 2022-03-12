14ef45adc769ce17b0c56f7253ca51b5f49c639f
MajedFakhrEldin <majed.ctb@gmail.com>
2022-03-12 01:40:01 +0100

    Returns data with the prefrences' units.

--- src/pages/data.js
+++ src/pages/data.js
@@ -1,13 +1,28 @@
-import useSWR from 'swr'
-import loadPreferences from '../lib/prefrences.js'
+import useSWR from 'swr';
+import {useContext} from 'react';
+import { PreferenceContext } from './_app';
+import {loadPreferences} from '../lib/loadPreferences.ts'
 
 const fetcher = (url) => fetch(url).then(res => res.json());
 const endpointUrl = "http://localhost:3000/api/v1?";
 
-export async function getServerSideProps(context){
-    const data = await fetcher(endpointUrl);
+function urlWithParams(url, params){
+    //A function that adds the given parameters to the given URL, params can be a string or an object for example.
+    return url + new URLSearchParams(params);
+}
 
-    pref = loadPrefrences(context.req.cookies.prefrences);
+export async function getServerSideProps(context){
+    //Since useContext(PreferenceContext) cannot be used:
+    const preferences = loadPreferences(context.req.cookies.preferences);
+    
+    const params = {
+        tempunit: preferences.temperature_unit.symbol,
+        conductunit: preferences.conductivity_unit.symbol
+    };
+
+    let url = urlWithParams(endpointUrl, params);
+    const data = await fetcher(url);
+    
     return{
         props: {
             initialData: data
@@ -17,14 +32,22 @@ export async function getServerSideProps(context){
 
 function App(props){
     const initialData = props.initialData;
+    const preferences = useContext(PreferenceContext);
 
-    const options = {fetcher: () => fetcher(endpointUrl),
+    const params = {
+        tempunit: preferences.temperature_unit.symbol,
+        conductunit: preferences.conductivity_unit.symbol
+    };
+
+    let url = urlWithParams(endpointUrl, params);
+
+    const options = {fetcher: () => fetcher(url),
                     fallbackData: initialData,
-                    refreshInterval: 1000 * 60}
-    const { data, error } = useSWR(endpointUrl, options);
+                    refreshInterval: 1000 * 60};
+    const { data, error } = useSWR(url, options);
 
     if (error) return <div>failed to load</div>;
-    if (!data) return <div>loading...</div>;
+    if (!data) return <div>loading...</div>;//Is this needed since there is already data?
     
     return <div>
         {data.map( (row) => (
