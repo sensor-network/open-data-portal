97bd19cc5425946422ea7d7e8db9718036491ef1
Julius <julius0216@outlook.com>
2022-03-14 19:19:30 +0100

    Added an AreaChart to visualize the data over time

---
+++ src/components/CustomAreaChart.jsx
@@ -0,0 +1,114 @@
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import {useContext, useState} from "react";
+import {AreaChart, Area, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
+import {PreferenceContext} from "src/pages/_app";
+
+import style from 'src/styles/AreaChart.module.css'
+
+const CustomAreaChart = ({ data }) => {
+    const preferences = useContext(PreferenceContext)
+    const [showTemperature, setShowTemperature] = useState(true);
+    const [showConductivity, setShowConductivity] = useState(false);
+    const [showPH, setShowPH] = useState(false);
+
+
+    const getMonthYear = (tick) => {
+        const date = new Date(tick);
+        return date.toLocaleString(undefined, {month: 'short', year: 'numeric'})
+    }
+
+    const CustomTooltip = ({ active, payload, label }) => {
+        if (active && payload && payload.length) {
+            const formattedDate = new Date(label).toLocaleString();
+            return (
+                <div className={style.chartToolTip}>
+                    <div className={style.dataLabel}>
+                        <h4>Measured at</h4>
+                        <p>{formattedDate}</p>
+                    </div>
+                    <div className={style.dataPayload}>
+                        {payload.map(row => {
+                            /** FIXME
+                             * shady way to extract the preference key from the `dataKey`-property,
+                             * also, take care for values not in the preferences
+                            **/
+                            let unit = preferences[`${row.dataKey}_unit`] && preferences[`${row.dataKey}_unit`].symbol;
+                            if (row.dataKey === 'temperature') unit = unit.toUpperCase();
+                            return (
+                                <p key={row.name}>
+
+                                    <strong>{row.name}:</strong> {row.value}{unit}
+                                </p>
+                            );
+                        })}
+                    </div>
+
+                </div>
+            );
+        }
+        return null;
+    };
+
+    return (
+        <>
+            <div style={{ width: '95%', maxWidth: 1000 }}>
+                <FormControlLabel
+                    onChange={(e) => setShowTemperature(e.target.checked)}
+                    control={<Checkbox defaultChecked />} label="Temperature"
+                />
+                <FormControlLabel
+                    onChange={(e) => setShowConductivity(e.target.checked)}
+                    control={<Checkbox />}
+                    label="Conductivity"
+                />
+                <FormControlLabel
+                    onChange={(e) => setShowPH(e.target.checked)}
+                    control={<Checkbox />}
+                    label="PH"
+                />
+            </div>
+
+            <div style={{ height: 750, width: '95%', maxWidth: 1000, paddingTop: 30 }}>
+                <ResponsiveContainer width='100%' height="80%" >
+                    <AreaChart data={data}
+                               margin={{ top: 0, right: 30, left: 0, bottom: 0 }}>
+                        <defs>
+                            <linearGradient id="colorTemp" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor="#1565c0" stopOpacity={0.8} />
+                                <stop offset="95%" stopColor="#1565c0" stopOpacity={0} />
+                            </linearGradient>
+                            <linearGradient id="colorPH" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor="#A83636FF" stopOpacity={0.8} />
+                                <stop offset="95%" stopColor="#A83636FF" stopOpacity={0} />
+                            </linearGradient>
+                            <linearGradient id="colorCond" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor="#A4C42FFF" stopOpacity={0.8}/>
+                                <stop offset="95%" stopColor="#A4C42FFF" stopOpacity={0}/>
+                            </linearGradient>
+                        </defs>
+                        <XAxis
+                            dataKey="date" tickFormatter={getMonthYear}
+                            interval={12} angle={-15} height={60}
+                            label={{value: `Measured At`, position: 'insideBottom' }}
+                        />
+                        <YAxis />
+                        <Legend verticalAlign="top" height={36}/>
+                        <Tooltip content={CustomTooltip} />
+                        {showTemperature &&
+                            <Area connectNulls type="monotone" dataKey="temperature" name="Temperature" stroke="#1565c0" fillOpacity={1} fill="url(#colorTemp)" />
+                        }
+                        {showPH &&
+                            <Area connectNulls type="monotone" dataKey="pH" stroke="#A83636FF" fillOpacity={1} fill="url(#colorPH)" />
+                        }
+                        {showConductivity &&
+                            <Area connectNulls type="monotone" dataKey="conductivity" name="Conductivity" stroke="#A4C42FFF" fillOpacity={1} fill="url(#colorCond)" />
+                        }
+                    </AreaChart>
+                </ResponsiveContainer>
+            </div>
+        </>
+    );
+};
+
+export default CustomAreaChart;
--- src/pages/data.js
+++ src/pages/data.js
@@ -1,8 +1,10 @@
 import useSWR from 'swr';
-import {useContext, useMemo} from 'react';
+import { useContext, useMemo } from 'react';
 import { PreferenceContext } from './_app';
-import {loadPreferences} from '../lib/loadPreferences.ts';
+import { loadPreferences } from '../lib/loadPreferences.ts';
 import { DataGrid } from '@mui/x-data-grid'; //Documentation can be found here: https://mui.com/api/data-grid/data-grid/
+
+import CustomAreaChart from 'src/components/CustomAreaChart';
 
 const fetcher = (url) => fetch(url).then(res => res.json());
 const endpointUrl = "http://localhost:3000/api/v1?";
@@ -65,7 +67,11 @@ export default function App(props){
     
       return(
           <>
-              <div style={{ display: 'flex', justifyContent: 'center', marginTop: 20 }}>
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 20 }}>
+                  <div style={{ width: '95%', maxWidth: 1000}}>
+                      <h2>Explore the data on your own</h2>
+                      <p>Change the units using the preference modal form the navbar. </p>
+                  </div>
                   <div style={{ height: 750, width: '95%', maxWidth: 1000 }}>
                       <DataGrid
                           rows= {data}
@@ -73,7 +79,16 @@ export default function App(props){
                       />
                   </div>
               </div>
+
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 20 }}>
+
+                  <div style={{ width: '95%', maxWidth: 1000}}>
+                      <h2>See how the data has changed over time</h2>
+                      <p>Select what datapoints you want to display in the grid using the checkboxes</p>
+                  </div>
+
+                  <CustomAreaChart data={data}/>
+              </div>
           </>
       );
 }
-
