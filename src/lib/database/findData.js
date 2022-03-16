1d5d5bba0ba3b395f3b22283512f65d3fec43971
Julius <julius0216@outlook.com>
2022-03-17 00:50:04 +0100

    refactored and made params optional

---
+++ src/lib/database/findData.js
@@ -0,0 +1,97 @@
+import { getConnectionPool } from "../database";
+const SRID = 4326;
+
+export const getRowCount = async () => {
+    const connection = await getConnectionPool();
+    const [ rowCount ] = await connection.query(
+        `SELECT count(*) FROM Data`
+    );
+    return rowCount;
+}
+
+export const findMany = async (location, params) => {
+    let data;
+    switch (location) {
+        case 'all':
+            data = await findAll(params);
+            break;
+        case 'by-geolocation':
+            data = await findAllByGeolocation(params);
+            break;
+        case 'by-location-name':
+            data = await findAllByLocationName(params);
+            break;
+        default:
+            throw Error('unknown location-filter')
+    }
+    return data;
+}
+
+const findAll = async (
+    { offset, page_size, start_date, end_date }
+) => {
+    const connection = await getConnectionPool();
+    const [ data ] = await connection.query(`
+        SELECT
+            id, date,
+            ST_Y(position) as longitude, ST_X(position) as latitude,
+            temperature, ph, conductivity
+        FROM
+            Data
+        WHERE
+            date >= ? AND date <= ?
+        ORDER BY
+            date
+        LIMIT
+            ?, ?
+    `, [
+        start_date, end_date,
+        offset, page_size
+    ]);
+    return data;
+}
+
+const findAllByLocationName = async (
+    /* TODO: Not implemented */
+    { name, offset, page_size, start_date, end_date }
+) => {
+    const connection = await getConnectionPool();
+    const [ data ] = await connection.query(`
+    `, [
+
+    ]);
+    return data;
+}
+
+const findAllByGeolocation = async (
+    { long, lat, rad, offset, page_size, start_date, end_date }
+) => {
+    const connection = await getConnectionPool();
+    const [ data ] = await connection.query(`
+        SELECT 
+            id,
+            pH,
+            temperature,
+            conductivity,
+            date,
+            ST_Y(position) as longitude,
+            ST_X(position) as latitude,
+            ST_Distance_Sphere(position, ST_GeomFromText('POINT(? ?)', ?, 'axis-order=long-lat')) as 'distance in meters'
+        FROM 
+            Data 
+        WHERE 
+            ST_Distance_Sphere(position, ST_GeomFromText('POINT(? ?)', ?, 'axis-order=long-lat')) < ?
+        AND
+            date >= ? AND date <= ?
+        ORDER BY 
+            Date
+        LIMIT 
+            ?, ?
+    `, [
+        long, lat, SRID,
+        long, lat, SRID, rad,
+        start_date, end_date,
+        offset, page_size
+    ]);
+    return data;
+}
--- src/pages/api/v1/bylocationanddate.js
+++ src/pages/api/v1/bylocationanddate.js
@@ -1,120 +1,55 @@
-import mysql from "mysql2/promise"
-import {z, ZodError} from "zod";
-import moment from "moment"
-
-import { getConnectionPool } from "src/lib/database";
+import { ZodError } from "zod";
 
 import {
   STATUS_OK,
   STATUS_BAD_REQUEST,
   STATUS_SERVER_ERROR
 } from "src/lib/httpStatusCodes";
-
-const LocationParams = z.object({
-    lat: z.preprocess(   // preprocess converts the string-query to a number
-        lat => Number(z.string().parse(lat)),   // validates the string could be parsed as number
-        z.number().gte(-90).lte(90)  // then validate the number is in valid range
-    ),
-    long: z.preprocess(
-        long => Number(z.string().parse(long)),
-        z.number().gte(-180).lte(180)
-    ),
-
-    rad: z.preprocess(
-        rad => Number(z.string().default("200").parse(rad)),
-        z.number().positive()
-    )
-});
-
-const TimeParams = z.object({
-  start_date: z.string().default('2022Z' /* new year 2022 */)
-      .refine(str => new Date(str).getTime() > 0, 'Unable to parse string as Date')
-      .refine(str => new Date(str) >= new Date('2022Z'), 'must be after 2022')
-      .transform(str => new Date(str).toISOString()),
-  end_date: z.string().default(new Date().toISOString() /* current time */)
-      .refine(str => new Date(str).getTime() > 0, 'Unable to parse string as Date')
-      .refine(str => new Date(str) <= new Date(), "can't be in the future")
-      .transform(str => new Date(str).toISOString()),
-}).refine(({start_date, end_date}) => end_date >= start_date, 'end_date must be before start_date');
-
-const PageParams = z.object({
-  page: z.preprocess(
-      page => Number(z.string().default("1").parse(page)),
-      z.number().positive()
-  ),
-  page_size: z.preprocess(
-      page_size => Number(z.string().default("100").parse(page_size)),
-      z.number().positive()
-  ),
-})
-
+import { getRowCount, findMany } from "src/lib/database/findData";
+import { zLocation, zTime, zPage } from 'src/lib/schemas/ZodSchemas';
  
 export default async function (req, res) {
-  try {
-    // Establish database connection and connect
-    const connection = await getConnectionPool();
+    try {
+        const { long, lat, rad, name } = zLocation.parse(req.query);
+        const { start_date, end_date } = zTime.parse(req.query);
+        let { page, page_size } = zPage.parse(req.query);
 
-    //query parameters
-    const SRID = 4326 //default spatial reference system
+        const rowCount = await getRowCount();
+        const last_page = Math.ceil(rowCount / page_size);
+        if (page > last_page)
+            page = last_page;
+        const offset = (page - 1) * page_size;  // last row of previous page
 
-    // Try parsing params using Zod schema
-    const {long, lat, rad} = LocationParams.parse(req.query); //location specifics
-    const {start_date, end_date} = TimeParams.parse(req.query); //date interval
+        let data;
+        if ( name ) {   // prioritize selecting by name
+            data = await findMany('by-location-name', {
+                name, start_date, end_date, offset, page_size
+            });
+        }
+        else if ( lat && long && rad ) {   // require both lat, long and rad to select by geolocation
+            data = await findMany('by-geolocation', {
+                lat, long, rad, start_date, end_date, offset, page_size
+            });
+        }
+        else {  // find all data if no location is specified
+            data = await findMany('all', {
+                start_date, end_date, offset, page_size
+            });
+        }
 
-    let { page, page_size } = PageParams.parse(req.query); //page number and page size
-    const [rows] = await connection.query('SELECT count(*) FROM Data');
-    const last_page = Math.ceil(rows / page_size);
-    if (page > last_page) page = last_page;
-    const offset = (page - 1) * page_size;
+        res.status(STATUS_OK).json({content: data});
+    }
 
-    
-    // Prepare the query
-    const query = mysql.format(`
-      SELECT 
-        id,
-        pH,
-        temperature,
-        conductivity,
-        date,
-        ST_Y(position) as longitude,
-        ST_X(position) as latitude,
-        ST_Distance_Sphere(position, ST_GeomFromText('POINT(? ?)', ?, 'axis-order=long-lat')) as 'distance in meters'
-      FROM 
-        Data 
-      WHERE 
-        ST_Distance_Sphere(position, ST_GeomFromText('POINT(? ?)', ?, 'axis-order=long-lat')) < ?
-      AND
-        date(date) >= ? AND date(date) <= ?
-      ORDER BY 
-        Date
-      Limit 
-        ?, ?
-    `, 
-      [
-        long, lat, SRID,
-        long, lat, SRID, rad,
-        start_date, end_date,
-        offset, page_size
-      ]
-    ); 
-
-    // Execute the query
-    const [data] = await connection.query(query);
-
-    // Respond with appropriate status code and json
-    res.status(STATUS_OK).json({content: data});
-  }
-
-  catch (e) {
-      if (e instanceof ZodError) {
-          console.log("Error parsing query params:\n", e.flatten())
-          res.status(STATUS_BAD_REQUEST)
-              .json(e.flatten());
-      }
-      else {
-          console.error(e)
-          res.status(STATUS_SERVER_ERROR).json({error: "Internal server error"})
-      }
-  }
+    catch (e) {
+        if (e instanceof ZodError) {
+            console.log("Error parsing query params:\n", e.flatten())
+            res.status(STATUS_BAD_REQUEST)
+                .json(e.flatten());
+        }
+        else {
+            console.error(e)
+            res.status(STATUS_SERVER_ERROR).json({error: "Internal server error"})
+        }
+    }
 } 
 
