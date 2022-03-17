0d3ca3603920a8ef1f736c1fdc54f54bb547ff8e
nilsperssonsuorra <82161471+nilsperssonsuorra@users.noreply.github.com>
2022-03-17 12:33:36 +0100

    Added swagger for /locations

--- src/pages/api/v1/locations.js
+++ src/pages/api/v1/locations.js
@@ -47,9 +47,6 @@ export default async function handler(req, res) {
         WHERE 
             name = ?;`,
         [params.name]);
-        
-        
-        console.log(query)
 
         const [data] = await connection.query(query);
 
