d3911fd30c6e7430dd965cdb83f804ce6d6ec075
Julius <julius0216@outlook.com>
2022-03-17 13:28:05 +0100

    fix return from getRowCount

--- src/lib/database/findData.js
+++ src/lib/database/findData.js
@@ -4,9 +4,9 @@ const SRID = 4326;
 export const getRowCount = async () => {
     const connection = await getConnectionPool();
     const [ rowCount ] = await connection.query(
-        `SELECT count(*) FROM Data`
+        `SELECT count(*) as row_count FROM Data`
     );
-    return rowCount;
+    return rowCount[0].row_count;
 }
 
 export const findMany = async (location, params) => {
@@ -41,7 +41,7 @@ const findAll = async (
         WHERE
             date >= ? AND date <= ?
         ORDER BY
-            date
+            id
         LIMIT
             ?, ?
     `, [
@@ -52,35 +52,32 @@ const findAll = async (
 }
 
 const findAllByLocationName = async (
-    /* TODO: Not implemented */
     { name, offset, page_size, start_date, end_date }
 ) => {
     const connection = await getConnectionPool();
     const [ data ] = await connection.query(`
-    SELECT 
-        id,
-        pH,
-        temperature,
-        conductivity,
-        date,
-        ST_Y(position) as longitude,
-        ST_X(position) as latitude
-    FROM 
-        Data 
-    AS 
-        d
-    WHERE 
-        ST_Distance_Sphere(d.position, (SELECT position from Locations where name = ?)) < (SELECT radius from Locations where name = ?)
-    AND
-        date(date) >= ? AND date(date) <= ?;
-    ORDER BY 
-        Date
-    LIMIT 
-        ?, ?
-  `, [
-    name, name,
-    start_date, end_date,
-    offset, page_size
+        SELECT 
+            id,
+            pH,
+            temperature,
+            conductivity,
+            date,
+            ST_Y(position) as longitude,
+            ST_X(position) as latitude
+        FROM 
+            Data AS d
+        WHERE 
+            ST_Distance_Sphere(d.position, (SELECT position from Locations where name = ?)) < (SELECT radius from Locations where name = ?)
+        AND
+            d.date >= ? AND d.date <= ?
+        ORDER BY 
+            d.date
+        LIMIT 
+            ?, ?
+    `, [
+        name, name,
+        start_date, end_date,
+        offset, page_size
     ]);
     return data;
 }
