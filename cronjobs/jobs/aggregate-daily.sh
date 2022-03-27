dffac97bbf40e409730c4d8e771a2d4b073f7789
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-03-27 19:28:59 +0200

    `bin/date`: select yesterday wihtout date-parser

--- cronjobs/jobs/aggregate-daily.sh
+++ cronjobs/jobs/aggregate-daily.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 # Script for aggregating the data for the day. Runs in silent mode and outputs a custom log to a monthly logfile.
 
+# 1 day = 86400 seconds
+YESTERDAY=$(date -d "@$(($(date +%s) - 86400))" +"%Y-%m-%d")
+
 # Options for the request (this job is triggered at midnight, hence aggregate yesterdays data)
-YESTERDAY=$(date --date="yesterday" +"%Y-%m-%d")
 URL="app:3000/api/private/aggregate-daily?date=$YESTERDAY"
 # URL="http://localhost:3000/api/private/aggregate-daily"
-HEADERS="Authorization: Basic default"
+HEADERS="Authorization: Bearer default"
 
 # Options for logging
 LOG_FILE="/var/log/aggregate-$(date +"%Y-%m").log"
