48518b8200c42b1f444f088c124ac0e4b34c0f41
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-03-28 16:14:38 +0200

    Made the old data stick around until the new one is loaded

--- src/components/ServerPaginationGrid.jsx
+++ src/components/ServerPaginationGrid.jsx
@@ -1,7 +1,10 @@
 import { useContext, useMemo, useState } from "react";
-import { PreferenceContext } from "../pages/_app";
 import useSWR from "swr";
 import { DataGrid } from "@mui/x-data-grid";
+import LinearProgress from "@mui/material/LinearProgress";
+
+import { laggy } from "../lib/hooks/swr-extensions";
+import { PreferenceContext } from "../pages/_app";
 
 const endpoint = "http://localhost:3000/api/v2/data?";
 const urlWithParams = (baseUrl, params) => baseUrl + new URLSearchParams(params);
@@ -10,28 +13,36 @@ const fetcher = async (url) => {
   return await response.json();
 };
 
-const ServerPaginationGrid = () => {
-  const [rowsState, setRowsState] = useState({
-    page: 0,
-    pageSize: 20,
+/* Wrapper for SWR using 'laggy' data */
+const useMeasurements = (url) => {
+  const { data, isLagging } = useSWR(url, {
+    fetcher: () => fetcher(url),
+    use: [laggy],
   });
+  return {
+    measurements: data?.data,
+    rowCount: data?.pagination.total_rows,
+    isLoading: isLagging,
+  };
+};
 
+
+const ServerPaginationGrid = () => {
+  /* Define pagination options, which can be modified in the grid */
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(20);
+
+  /* Get correct url for fetching the filtered data */
   const { preferences } = useContext(PreferenceContext);
   const url = useMemo(() => urlWithParams(endpoint, {
     temperature_unit: preferences.temperature_unit.symbol,
     conductivity_unit: preferences.conductivity_unit.symbol,
-    page: rowsState.page + 1,
-    page_size: rowsState.pageSize,
-  }), [preferences, rowsState]);
+    location_name: preferences.location.symbol,
+    page: page + 1, /* mui grid starts indexing at 0, api at 1 */
+    page_size: pageSize,
+  }), [preferences, page, pageSize]);
 
-  const { data, isLoading } = useSWR(url, {
-    fetcher: () => fetcher(url),
-    fallbackData: { pagination: { total_rows: 0 }, data: [] },
-    refreshInterval: 1000 * 60,
-  });
-  const rows = data.data;
-  const rowCount = data.pagination.total_rows;
-
+  const { measurements, rowCount, isLoading } = useMeasurements(url);
 
   const gridColumns = useMemo(() => [
     { field: "id", headerName: "ID", width: 70, editable: false },
@@ -60,17 +71,18 @@ const ServerPaginationGrid = () => {
   return (
     <div style={{ height: 750, width: "95%", maxWidth: 1000 }}>
       <DataGrid
-        rows={rows}
+        rows={measurements}
         columns={gridColumns}
         rowCount={rowCount}
         loading={isLoading}
+        components={{ LoadingOverlay: LinearProgress }}
         pagination
         paginationMode={"server"}
-        page={rowsState.page}
-        pageSize={rowsState.pageSize}
+        page={page}
+        pageSize={pageSize}
         rowsPerPageOptions={[5, 10, 20, 50, 100]}
-        onPageChange={(page) => setRowsState((prev) => ({ ...prev, page }))}
-        onPageSizeChange={(pageSize) => setRowsState((prev) => ({ ...prev, pageSize }))}
+        onPageChange={page => setPage(page)}
+        onPageSizeChange={pageSize => setPageSize(pageSize)}
       />
     </div>
   );
