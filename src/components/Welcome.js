0e947da6a91964998e9825cd004958e860d9cb0f
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-03-30 01:21:40 +0200

    some styling

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,25 +1,27 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Card from "src/components/Card";
+
 function Welcome() {
-    return (
-        <div className={"container"}>
-            <div>
-                <h1>Welcome</h1>
-            </div>
-            <div>
-                This application is a work in progress, including:
+  return (
+    <div style={{ padding: 20, width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <Card title="Welcome">
+        <div>
+          <p>This application is a work in progress, including:</p>
 
-                    <p>A basic API</p>
-                    <p>API Documentation</p>
-                    <p>Data Grid for exploring data</p>
-                    <p>Basic overview diagram of the data</p>
+          <p>- A basic API</p>
+          <p>- API Documentation</p>
+          <p>- Data Grid for exploring data</p>
+          <p>- Basic overview diagram of the data</p>
 
-                Explore using the tabs in the navbar, and change the unit which the data is presented in using the preference modal.
-            </div>
-            <div>
-                <Image src={"/bth.png"} width={400} height={400}/>
-            </div>
+          <p>Explore using the tabs in the navbar, and change the unit which the data
+            is presented in using the preference modal.</p>
         </div>
-    );
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Image src={"/bth.png"} width={400} height={400}/>
+        </div>
+      </Card>
+    </div>
+  );
 }
 
 export default Welcome;
