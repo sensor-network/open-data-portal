23c645b0c76d5eb269209a93f0462db042ed8122
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-01 20:46:29 +0200

    use siemens per meter as default conducitivty unit
    usage of this unit makes graphing its correlations to other sensors easier, since ppm is ~30.000

--- src/lib/loadPreferences.ts
+++ src/lib/loadPreferences.ts
@@ -1,15 +1,20 @@
-import {UNITS as TEMP_UNITS} from "lib/units/temperature";
-import {UNITS as COND_UNITS} from "lib/units/conductivity";
+import { UNITS as TEMP_UNITS } from "lib/units/temperature";
+import { UNITS as COND_UNITS } from "lib/units/conductivity";
 
 export const loadPreferences = (prefCookieString: string) => {
-    /* load preferences from cookies, or fallback to default values */
-    let json;
-    try {
-        json = JSON.parse(prefCookieString);
-    } catch (e) {}
-    return {
-        location: json?.location || { name: 'Everywhere', symbol: 'all' },
-        temperature_unit:  json?.temperature_unit ||  { name: TEMP_UNITS.CELSIUS.name,           symbol: TEMP_UNITS.CELSIUS.symbol },
-        conductivity_unit: json?.conductivity_unit || { name: COND_UNITS.PARTS_PER_MILLION.name, symbol: COND_UNITS.PARTS_PER_MILLION.symbols[0] }
+  /* load preferences from cookies, or fallback to default values */
+  let json;
+  try {
+    json = JSON.parse(prefCookieString);
+  }
+  catch (e) {
+  }
+  return {
+    location: json?.location || { name: 'Everywhere', symbol: 'all' },
+    temperature_unit: json?.temperature_unit || { name: TEMP_UNITS.CELSIUS.name, symbol: TEMP_UNITS.CELSIUS.symbol },
+    conductivity_unit: json?.conductivity_unit || {
+      name: COND_UNITS.SIEMENS_PER_METER.name,
+      symbol: COND_UNITS.SIEMENS_PER_METER.symbols[0]
     }
+  };
 };
