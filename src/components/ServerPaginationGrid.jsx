a8ae181630c5d67a74a697b13a223a52e7423fa5
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-01 13:11:29 +0200

    Placed `ServerPaginationGrid` inside a `Card` and added a `DateRangeSelector` to it.

--- src/components/ServerPaginationGrid.jsx
+++ src/components/ServerPaginationGrid.jsx
@@ -1,21 +1,28 @@
 import { useContext, useMemo, useState } from "react";
+
 import { DataGrid } from "@mui/x-data-grid";
 import LinearProgress from "@mui/material/LinearProgress";
+import Card from "src/components/Card";
+import DateRangeSelector from "src/components/DateRangeSelector";
 
 import { useMeasurements } from "../lib/hooks/swr-extensions";
 import { PreferenceContext } from "../pages/_app";
-import {fetcher, urlWithParams} from "../lib/utilityFunctions";
+import { fetcher, urlWithParams } from "../lib/utilityFunctions";
 
-const endpoint = "http://localhost:3000/api/v2/data?";
+const ENDPOINT = "http://localhost:3000/api/v2/data?";
 
 const ServerPaginationGrid = () => {
   /* Define pagination options, which can be modified in the grid */
   const [page, setPage] = useState(0);
   const [pageSize, setPageSize] = useState(20);
 
+  /* Define date range, which can be modified in the date range selector */
+  const [startDate, setStartDate] = useState(new Date(1));
+  const [endDate, setEndDate] = useState(new Date());
+
   /* Get correct url for fetching the filtered data */
   const { preferences } = useContext(PreferenceContext);
-  const url = useMemo(() => urlWithParams(endpoint, {
+  const url = useMemo(() => urlWithParams(ENDPOINT, {
     temperature_unit: preferences.temperature_unit.symbol,
     conductivity_unit: preferences.conductivity_unit.symbol,
     location_name: preferences.location.symbol,
@@ -26,8 +33,8 @@ const ServerPaginationGrid = () => {
   const { measurements, rowCount, isLoading } = useMeasurements(url, fetcher);
 
   const gridColumns = useMemo(() => [
-    { field: "id", headerName: "ID", width: 70},
-    { field: "ph", headerName: "pH", width: 90},
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "ph", headerName: "pH", width: 90 },
     {
       field: "temperature",
       headerName: `Temperature (${preferences.temperature_unit.symbol})`,
@@ -36,34 +43,46 @@ const ServerPaginationGrid = () => {
     {
       field: "conductivity",
       headerName: `Conductivity (${preferences.conductivity_unit.symbol})`,
-      width: 150
+      width: 150,
     },
     {
       field: "date", width: 200,
       headerName: `Date (${Intl.DateTimeFormat().resolvedOptions().locale})`,
       valueGetter: date => new Date(date.value).toLocaleString(),
     },
-    { field: "longitude", headerName: "Longitude", width: 150},
-    { field: "latitude", headerName: "Latitude", width: 150},
+    { field: "longitude", headerName: "Longitude", width: 150 },
+    { field: "latitude", headerName: "Latitude", width: 150 },
   ], [preferences]);
 
   return (
-    <div style={{ height: 750, width: "95%", maxWidth: 1000 }}>
-      <DataGrid
-        rows={measurements}
-        columns={gridColumns}
-        rowCount={rowCount}
-        loading={isLoading}
-        components={{ LoadingOverlay: LinearProgress }}
-        pagination
-        paginationMode={"server"}
-        page={page}
-        pageSize={pageSize}
-        rowsPerPageOptions={[5, 10, 20, 50, 100]}
-        onPageChange={page => setPage(page)}
-        onPageSizeChange={pageSize => setPageSize(pageSize)}
-      />
-    </div>
+    <Card title="Explore the data on your own" margin="40px 0 0 0">
+
+      <div style={{ height: 750, margin: "20px 0" }}>
+        <DataGrid
+          rows={measurements}
+          columns={gridColumns}
+          rowCount={rowCount}
+          loading={isLoading}
+          components={{ LoadingOverlay: LinearProgress }}
+          pagination
+          paginationMode={"server"}
+          page={page}
+          pageSize={pageSize}
+          rowsPerPageOptions={[5, 10, 20, 50, 100]}
+          onPageChange={page => setPage(page)}
+          onPageSizeChange={pageSize => setPageSize(pageSize)}
+        />
+      </div>
+
+      <div>
+        <DateRangeSelector
+          startDate={startDate} setStartDate={setStartDate}
+          endDate={endDate} setEndDate={setEndDate}
+        />
+      </div>
+
+    </Card>
+
   );
 };
 
