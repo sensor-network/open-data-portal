83189faa5d7d781741450eb4fbd9c8bafb435469
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-07 11:51:05 +0200

    typed useLocations-hook

--- src/lib/hooks/useLocations.ts
+++ src/lib/hooks/useLocations.ts
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
+import type { Location } from 'src/lib/database/location';
 
 export const useLocations = (url: string) => {
-  const [locations, setLocations] = useState();
+  const [locations, setLocations] = useState<Array<Location> | undefined>(undefined);
 
   useEffect(() => {
     const fetchLocations = async () => {
       const res = await fetch(url);
-      const locations = await res.json();
+      const locations: Array<Location> = await res.json();
       setLocations(locations);
     };
     fetchLocations();
