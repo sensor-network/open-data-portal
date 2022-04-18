3900e3cb61c0bcba2e466dd23406a7e333f10271
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-18 20:38:10 +0200

    GET/POST/PATCH /api/v3/locations

--- src/lib/types/ZodSchemas.ts
+++ src/lib/types/ZodSchemas.ts
@@ -4,36 +4,13 @@ import { isValid } from 'date-fns';
 
 
 /* GENERAL SCHEMAS */
+/* used when parsing an id from query param */
 export const zIdFromString = z.string()
   .transform(str => parseInt(str, 10))
   .refine((num) => num > 0, 'id has to be positive');
 
+/* used when parsing an id from body param */
 export const zId = z.number().int().positive();
-
-/**
- * schema for parsing location information,
- * geo-information is validated to be in range
- **/
-export const zLocation = z.object({
-  lat: z.optional(z.string()  // input is a string which has to be transformed
-    .transform(str => Number(str))
-    .refine((num) => num >= -90, 'should be greater than or equal to -90')
-    .refine((num) => num <= 90, 'should be less than or equal to 90')
-    .or(z.number().gte(-90).lte(90)) // no need to transform if input is already a number (e.g. when coming from req. body)
-  ),
-  long: z.optional(z.string()
-    .transform(str => Number(str))
-    .refine((num) => num >= -180, 'should be greater than or equal to -180')
-    .refine((num) => num <= 180, 'should be less than or equal to 180')
-    .or(z.number().gte(-180).lte(180)) // no need to transform if input is already a number (e.g. when coming from req. body)
-  ),
-  rad: z.string().optional().default('200')
-    .transform(str => Number(str))
-    .refine((num) => num > 0, 'should be positive')
-    .or(z.number().positive())  // no need to transform if input is already a number (e.g. when coming from req. body)
-  ,
-  locationName: z.string().optional(),
-});
 
 /**
  * schema for parsing a valid time range
@@ -65,6 +42,54 @@ export const zPage = z.object({
     z.number().int().positive()
   ),
 });
+
+
+/* LOCATION SCHEMAS */
+/**
+ * schema for creating a location entry.
+ * since params comes from body, no need to convert from string
+ **/
+export const zCreateLocation = z.object({
+  lat: z.number().gte(-90).lte(90),
+  long: z.number().gte(-180).lte(180),
+  rad: z.number().positive(),
+  name: z.string()
+}).strict();
+
+export const zUpdateLocation = z.object({
+  name: z.string().optional().nullable(),
+}).strict();
+/**
+ * schema for parsing location information,
+ * geo-information is validated to be in range.
+ * params comes from query string, so need to convert from string
+ **/
+export const zLocation = z.object({
+  lat: z.optional(z.string()
+    .transform(str => Number(str))
+    .refine((num) => num >= -90, 'should be greater than or equal to -90')
+    .refine((num) => num <= 90, 'should be less than or equal to 90')),
+  long: z.optional(z.string()
+    .transform(str => Number(str))
+    .refine((num) => num >= -180, 'should be greater than or equal to -180')
+    .refine((num) => num <= 180, 'should be less than or equal to 180')),
+  rad: z.preprocess(rad => {
+    /* radius is optional, if it is left out, it should be null */
+    if (rad === undefined) {
+      return null;
+    }
+    return Number(rad);
+  }, z.number().int().positive().nullable()),
+  name: z.optional(z.string()),
+})
+  .refine(({ lat, long }) => {
+    /* if one of them are specified, both have to be specified */
+    if (lat !== undefined || long !== undefined) {
+      return lat !== undefined && long !== undefined;
+    }
+    /* but they can both be left out */
+    return true;
+  }, 'lat and long must be provided together');
 
 
 /* MEASUREMENT SCHEMA */
