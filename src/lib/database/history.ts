325b8bf843e2a93e38a118414cb77938bfd8a684
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-18 20:59:34 +0200

    Small adjustments for /api/private/aggregate-daily

--- src/lib/database/history.ts
+++ src/lib/database/history.ts
@@ -13,12 +13,12 @@ export type CombinedFormat = {
 export const createOne = async (
   {
     date,
-    sensor_type,
-    location_id,
-  }: { date: string | Date, sensor_type: string, location_id: number },
+    sensorType,
+    locationId,
+  }: { date: string | Date, sensorType: string, locationId: number },
 ) => {
   const connection = await getConnectionPool();
-  const [result] = await connection.query(`
+  const [result, _]: [result: OkPacket, _: any] = await connection.query(`
       INSERT INTO history
           (date, location_id, type, daily_min, daily_avg, daily_max)
       VALUES (?, ?, ?,
@@ -26,66 +26,75 @@ export const createOne = async (
               (SELECT AVG(value) FROM measurement WHERE date(time) = ? AND location_id = ? AND type = ?),
               (SELECT MAX(value) FROM measurement WHERE date(time) = ? AND location_id = ? AND type = ?))
   `, [
-    date, location_id, sensor_type,
-    date, location_id, sensor_type,
-    date, location_id, sensor_type,
-    date, location_id, sensor_type,
+    date, locationId, sensorType,
+    date, locationId, sensorType,
+    date, locationId, sensorType,
+    date, locationId, sensorType,
   ]);
-  return (<OkPacket>result).insertId;
+  return result.insertId;
 };
 
 /* query for getting data formatted the same way for history and measurement table */
 export const findInCombinedFormat = async (
   {
-    start_date,
-    end_date,
-    location_id
-  }: { start_date: Date | string, end_date: Date | string, location_id: number },
+    startDate,
+    endDate,
+    locationId
+  }: { startDate: Date | string, endDate: Date | string, locationId: number },
 ) => {
-  /* if location_id = -1 we select all, else select with id */
-  const query = location_id > 0 ? mysql.format(`
+  /* if locationId = -1 we select all, else select with id */
+  const query = locationId > 0
+    ? mysql.format(`
               SELECT type, daily_avg as avg, date as time, daily_min as min, daily_max as max
               FROM history
               WHERE location_id = ?
                 AND date BETWEEN ? AND ?
-    `, [location_id, start_date, end_date]) :
-    mysql.format(`
-        SELECT type, daily_avg as avg, date as time, daily_min as min, daily_max as max
-        FROM history
-        WHERE date BETWEEN ? AND ?
-    `, [start_date, end_date]);
+    `, [
+      locationId, startDate, endDate
+    ])
+    : mysql.format(`
+              SELECT type, daily_avg as avg, date as time, daily_min as min, daily_max as max
+              FROM history
+              WHERE date BETWEEN ? AND ?
+    `, [
+      startDate, endDate
+    ]);
   const connection = await getConnectionPool();
-  const [result] = await connection.query(query);
-  return <RowDataPacket[]>result as Array<CombinedFormat>;
+  const [result, _]: [result: RowDataPacket[], _: any] = await connection.query(query);
+  return result as CombinedFormat[];
 };
 
 export const findMany = async (
   {
-    start_date,
-    end_date,
-    location_id
-  }: { start_date: string | Date, end_date: string | Date, location_id: number },
+    startDate,
+    endDate,
+    locationId
+  }: { startDate: string | Date, endDate: string | Date, locationId: number },
 ) => {
   const connection = await getConnectionPool();
-  const [result] = await connection.query(`
+  const [result, _]: [result: RowDataPacket[], _: any] = await connection.query(`
       SELECT *
       FROM history
       WHERE date BETWEEN ? AND ?
         AND location_id = ?
-  `, [start_date, end_date, location_id]);
-  return <RowDataPacket[]>result;
+  `, [startDate, endDate, locationId]);
+  return result;
 };
 
 export const findByFilter = async (
-  { date, sensor_type, location_id }: { date: string | Date, sensor_type: string, location_id: number },
+  {
+    date,
+    sensorType,
+    locationId
+  }: { date: string | Date, sensorType: string, locationId: number },
 ) => {
   const connection = await getConnectionPool();
-  const [result] = await connection.query(`
+  const [result, _]: [result: RowDataPacket[], _: any] = await connection.query(`
       SELECT *
       FROM history
       WHERE date = ?
         AND location_id = ?
         AND type = ?
-  `, [date, location_id, sensor_type]);
-  return <RowDataPacket[]>result;
+  `, [date, locationId, sensorType]);
+  return result;
 };
