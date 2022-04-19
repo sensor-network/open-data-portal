f90c7207992f43ba4ea18e8d95c9ea051c841687
MajedFakhrEldin <majed.ctb@gmail.com>
2022-04-19 17:21:10 +0200

    Began adding dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,10 @@
+import Card from "src/components/Card";
+
+
+export default function Dashboard(){
+    return(
+        <Card title = "Current Temperature">
+            <p>hello</p>
+        </Card>
+    )
+}
--- src/pages/data.js
+++ src/pages/data.js
@@ -1,12 +1,15 @@
 import Container from "@mui/material/Container";
 
 import ServerPaginationGrid from "src/components/ServerPaginationGrid";
+import Dashboard from "src/components/Dashboard";
 import Visualization from "src/components/Visualization";
 import Summary from "src/components/Summary";
 
 export default function App() {
   return (
     <Container style={{ display: "flex", flexDirection: "column", gap: 50, marginBottom: 50 }} maxWidth={"xl"}>
+
+      <Dashboard/>
 
       <ServerPaginationGrid/>
 
