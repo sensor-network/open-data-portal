cf2eeb98fbf93c7922c42f5d9358e3ef1f54eb13
MajedFakhrEldin <majed.ctb@gmail.com>
2022-04-21 11:00:26 +0200

    Added endpoint

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,13 +3,15 @@ import { useLocations } from "src/lib/hooks/useLocations";
 
 export default function Dashboard(){
 
+    const ENDPOINT = "http://localhost:3000/api/v2/measurements?"; // Change to v3 later
+
     const locations = useLocations("/api/v2/locations"); // Change to v3 later
     console.log(locations);
     return(
         <Card title = "Current Temperature">
             <>
                 {locations?.map(location => (
-                   <div key={location.id}>{location.name}</div> 
+                   <div key={location.id}>{location.name} {location.name}</div> 
                 ))}
             </>
         </Card>
