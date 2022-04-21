997acf3f1dcf7bf08bb82d3ae33a4e2a0eca8626
MajedFakhrEldin <majed.ctb@gmail.com>
2022-04-21 09:13:29 +0200

    Beginning to add locations

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,17 @@
 import Card from "src/components/Card";
-
+import { useLocations } from "src/lib/hooks/useLocations";
 
 export default function Dashboard(){
+
+    const locations = useLocations("/api/v2/locations"); // Change to v3 later
+    console.log(locations);
     return(
         <Card title = "Current Temperature">
-            <p>hello</p>
+            <>
+                {locations?.map(location => (
+                   <div key={location.id}>{location.name}</div> 
+                ))}
+            </>
         </Card>
     )
 }
