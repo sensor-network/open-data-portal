ecf5a09e063b2bb5e8960f9d52d59cfe8953f980
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-21 23:34:43 +0200

    bring back summarizeValues

--- src/lib/utilityFunctions.js
+++ src/lib/utilityFunctions.js
@@ -52,6 +52,26 @@ export const dateFormatter = (date, startDate, endDate) => {
   return format(parsed, "d MMM");
 };
 
+/* take in an array of values and return the min, max and avg */
+ export const summarizeValues = (values, decimals = 2) => {
+   let [minIndex, maxIndex, sum] = [0, 0, 0];
+   for (let i = 0; i < values.length; i++) {
+     const value = values[i];
+     if (value < values[minIndex]) {
+       minIndex = i;
+     }
+     if (value > values[maxIndex]) {
+       maxIndex = i;
+     }
+     sum += value;
+   }
+   return {
+     min: round(values[minIndex], decimals),
+     max: round(values[maxIndex], decimals),
+     avg: round(sum / values.length, decimals),
+   };
+ };
+
 /* round given value to specified precision */
 export const round = (value, decimals = DECIMAL_PLACES) => {
   const roundFactor = Math.pow(10, decimals);
