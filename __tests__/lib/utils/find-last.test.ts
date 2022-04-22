34a3b51c0e48480ca8040aa86e6d459bf7bba415
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-22 10:36:24 +0200

    add find-last test 3

--- __tests__/lib/utils/find-last.test.ts
+++ __tests__/lib/utils/find-last.test.ts
@@ -4,11 +4,11 @@ type SimpleObject = {
   id: number;
   value: string;
 };
-const callback = (arg: SimpleObject) => arg.id === 1;
 
 describe("TEST findLast", () => {
   it("TEST 1 - returns null on an empty array", () => {
     const array: SimpleObject[] = [];
+    const callback = (arg: SimpleObject) => arg.id === 1;
 
     const result = findLast(array, callback);
 
@@ -22,9 +22,24 @@ describe("TEST findLast", () => {
       { id: 1, value: "1.1" },
       { id: 3, value: "3" },
     ];
+    const callback = (arg: SimpleObject) => arg.id === 1;
 
     const result = findLast(array, callback);
 
     expect(result).toEqual({ id: 1, value: "1.1" });
+  });
+
+  it("TEST 3 - the element is not in the array", () => {
+    const array: SimpleObject[] = [
+      { id: 1, value: "1" },
+      { id: 2, value: "2" },
+      { id: 1, value: "1.1" },
+      { id: 3, value: "3" },
+    ];
+    const callback = (arg: SimpleObject) => arg.id === 4;
+
+    const result = findLast(array, callback);
+
+    expect(result).toEqual(null);
   });
 });
