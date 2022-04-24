8240518fca3fec528742814c435c448a327a9e53
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-24 18:59:33 +0200

    test GET /api/v3/locations

--- __tests__/pages/api/v3/locations/index.test.js
+++ __tests__/pages/api/v3/locations/index.test.js
@@ -1,6 +1,11 @@
 import { createMocks } from "node-mocks-http";
 import handler from "~/pages/api/v3/locations/index";
-import { findByName } from "~/lib/database/location";
+import {
+  createOne,
+  findMany,
+  findByName,
+  findByLatLong,
+} from "~/lib/database/location";
 
 const locationDb = [
   { id: 1, name: "foo", position: { lat: 0, long: 0 }, radiusMeters: 100 },
@@ -10,6 +15,7 @@ const locationDb = [
 
 jest.mock("~/lib/database/location", () => ({
   __esModule: true,
+  // createOne:
   findMany: jest.fn().mockImplementation(async () => locationDb),
   findByName: jest
     .fn()
@@ -26,6 +32,10 @@ jest.mock("~/lib/database/location", () => ({
     ),
 }));
 
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 describe("GET: /api/v3/locations", () => {
   const mockReqRes = ({ method = "GET", query = {} }) => {
     return createMocks({
@@ -34,14 +44,40 @@ describe("GET: /api/v3/locations", () => {
     });
   };
 
+  it("should return 400 if called with invalid query", async () => {
+    const { req, res } = mockReqRes({ query: { name: 1 } });
+
+    await handler(req, res);
+
+    expect(findMany.mock.calls.length).toEqual(0);
+    expect(findByName.mock.calls.length).toEqual(0);
+    expect(findByLatLong.mock.calls.length).toEqual(0);
+    expect(res.statusCode).toBe(400);
+  });
+
   describe("no filters", () => {
     it("should return a list of locations", async () => {
       const { req, res } = mockReqRes({});
 
       await handler(req, res);
 
+      expect(findMany.mock.calls.length).toEqual(1);
       expect(res._getStatusCode()).toEqual(200);
       expect(res._getJSONData()).toEqual(locationDb);
+    });
+
+    it("should return 404 if no location is found", async () => {
+      const { req, res } = mockReqRes({});
+
+      findMany.mockImplementation(async () => []);
+
+      await handler(req, res);
+
+      expect(findMany.mock.calls.length).toEqual(1);
+      expect(res._getStatusCode()).toEqual(404);
+      expect(res._getJSONData()).toEqual({
+        message: expect.stringContaining("No location"),
+      });
     });
   });
 
@@ -51,19 +87,45 @@ describe("GET: /api/v3/locations", () => {
 
       await handler(req, res);
 
+      expect(findByName.mock.calls.length).toEqual(1);
       expect(res._getStatusCode()).toEqual(200);
       expect(res._getJSONData()).toEqual([locationDb[0]]);
+    });
+
+    it("should return 404 if no locations matched", async () => {
+      const { req, res } = mockReqRes({ query: { name: "foobar" } });
+
+      await handler(req, res);
+
+      expect(findByName.mock.calls.length).toEqual(1);
+      expect(res._getStatusCode()).toEqual(404);
+      expect(res._getJSONData()).toEqual({
+        message: expect.stringContaining("No location"),
+      });
     });
   });
 
   describe("filter by lat/lng", () => {
-    it("should filter by lat/lng if provided", async () => {
+    it("should return the matched locations", async () => {
       const { req, res } = mockReqRes({ query: { lat: "10", long: "10" } });
 
       await handler(req, res);
 
+      expect(findByLatLong.mock.calls.length).toEqual(1);
       expect(res._getStatusCode()).toEqual(200);
       expect(res._getJSONData()).toEqual([locationDb[1]]);
+    });
+
+    it("should return 404 if no locations matched", async () => {
+      const { req, res } = mockReqRes({ query: { lat: "-10", long: "-10" } });
+
+      await handler(req, res);
+
+      expect(findByLatLong.mock.calls.length).toEqual(1);
+      expect(res._getStatusCode()).toEqual(404);
+      expect(res._getJSONData()).toEqual({
+        message: expect.stringContaining("No location"),
+      });
     });
   });
 });
