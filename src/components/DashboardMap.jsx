372bf57e8e3ca10865ea8e8f4cb14d5690216c88
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-25 23:53:40 +0200

    refactor icon-config

--- src/components/DashboardMap.jsx
+++ src/components/DashboardMap.jsx
@@ -4,26 +4,24 @@ import "leaflet/dist/images/marker-shadow.png";
 import "leaflet/dist/images/marker-icon.png";
 import { Icon } from "leaflet";
 
-const blueIcon = new Icon({
-  iconUrl:
-    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png",
+const iconConfig = {
   shadowUrl:
     "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   shadowSize: [41, 41],
+};
+
+const blueIcon = new Icon({
+  ...iconConfig,
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png",
 });
 
 const redIcon = new Icon({
   iconUrl:
     "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
 const DashboardMap = ({ locations, selectedLocation, mapCenter }) => {
