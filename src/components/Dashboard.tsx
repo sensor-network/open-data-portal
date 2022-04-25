a3b951cbac564eca5cb5cf4b1b0e95c9f87040b8
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-25 23:51:54 +0200

    remove right-component since it was unnecessary

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,36 +1,42 @@
 import Card from "src/components/Card";
 import Left from "src/components/DashboardLeft";
-import Right from "src/components/DashboardRight";
 import { useLocations } from "src/lib/hooks/useLocations";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+
+/* load map without ssr due to lack of support with Leaflet */
+const MapWithNoSSR = dynamic(() => import("../components/DashboardMap"), {
+  ssr: false,
+});
 
 const paneStyle = {
   width: "50%",
-  minHeight: 200,
+  minHeight: "80vh",
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
 };
 
+const mapCenter = [56.178516, 15.60261];
+
 const Dashboard: React.FC = () => {
-  
   const locations = useLocations("/api/v3/locations");
   const TIMEOUT_MS = 5000;
 
-  const [selectedIdx, setSelectedIdx] = useState(0);
+  const [selectedLocationIndex, setSelectedIndex] = useState(0);
 
-    useEffect(() => {
-      if (locations) {
-        console.log("setting timeout")
-        setInterval(() => {
-          setSelectedIdx((prev) => {
-            if (prev === locations.length -1) 
-              return 0;
-            return prev + 1;
-          });
-        }, TIMEOUT_MS);
-      }
-    }, [locations]);
+  useEffect(() => {
+    /* initialize an interval when locations arrive */
+    if (locations) {
+      setInterval(() => {
+        /* on every iteration, increment the selected location with wrap-around */
+        setSelectedIndex((prev) => {
+          if (prev === locations.length - 1) return 0;
+          return prev + 1;
+        });
+      }, TIMEOUT_MS);
+    }
+  }, [locations]);
 
   return (
     <Card title="Dashboard" margin={20}>
@@ -38,10 +44,17 @@ const Dashboard: React.FC = () => {
         {locations ? (
           <>
             <div style={paneStyle}>
-              <Left locations={locations} selectedLocation={selectedIdx}/>
+              <Left
+                locations={locations}
+                selectedLocation={selectedLocationIndex}
+              />
             </div>
             <div style={paneStyle}>
-              <Right locations={locations} selectedLocation={selectedIdx}/>
+              <MapWithNoSSR
+                locations={locations}
+                selectedLocation={selectedLocationIndex}
+                mapCenter={mapCenter}
+              />
             </div>
           </>
         ) : (
