3a3318dde3664232f5d5ba58c97698fd296fef9a
nilsperssonsuorra <82161471+nilsperssonsuorra@users.noreply.github.com>
2022-04-26 16:55:47 +0200

    Update AdminDashboard.jsx
    * Now shows in the overview sensor status if one or multiple sensors failed.

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -11,14 +11,7 @@ const healthData = [
     status: 1.0,
     lastCheckTime: 1611990000000,
     datapoints: [
-      { timestamp: '1611964800000', value: 0.5 },
-      { timestamp: '1611968400000', value: 1.0 },
-      { timestamp: '1611972000000', value: 1.0 },
-      { timestamp: '1611975600000', value: 1.0 },
-      { timestamp: '1611979200000', value: 0.9996438888888889 },
-      { timestamp: '1611982800000', value: 1.0 },
-      { timestamp: '1611986400000', value: 1.0 },
-      { timestamp: '1611990000000', value: 1.0 }
+
     ],
     elements: [
 
@@ -39,11 +32,12 @@ async function fetchSensordata() {
 
     if (sensorData[i].status == "ok") {
       emptylist.push({"name":sensorData[i].name, "status": 1.0, lastCheckTime: sensorData[i].lastActive, datapoints: []})
-      //healthData[0].elements?.push({timestamp: sensorData[i].lastActive, value: 1.0})
+      healthData[0].lastCheckTime = sensorData[i].lastActive
     }
 
     else {
       emptylist.push({"name":sensorData[i].name, "status": 0.0, lastCheckTime: sensorData[i].lastActive, datapoints: []})
+      healthData[0].lastCheckTime = sensorData[i].lastActive
     }
 
     
@@ -51,16 +45,25 @@ async function fetchSensordata() {
 
   for (var i = 0; i < emptylist.length; i++) {
     healthData[0].elements?.push(emptylist[i])}
-    console.log("emptylist is:")
-    console.log(emptylist[i])
-    console.log(emptylist)
+
+  for (let i = 0; i < healthData[0].elements.length; i++) {
+    console.log(healthData[0].elements[i].status)
+    if (healthData[0].elements[i].status === 0)
+    {
+      healthData[0].status = 0.5
+    }
+  }
   //for (var i = 0; i < emptylist.length; i++) {
   //  healthData[0].datapoints?.push(emptylist[i])}
+  console.log("logging")
+  console.log(healthData)
 
 }
 
 
 fetchSensordata()
+
+
 
 const AdminView = () => {
 
