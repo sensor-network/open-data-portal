9c34d088e0c4dab1c6a9f8e67f57918246090a7e
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-26 08:14:33 +0200

    can you infer the type now?

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -9,11 +9,7 @@ import type { Location } from "src/lib/database/location";
 import dynamic from "next/dynamic";
 const MapWithNoSSR = dynamic(() => import("./DashboardMap"), {
   ssr: false,
-}) as React.FC<{
-  locations: Location[];
-  selectedLocation: number;
-  mapCenter: PointTuple;
-}>;
+});
 
 const paneStyle = {
   width: "50%",
