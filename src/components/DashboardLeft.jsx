1d7e6cf6fa25faac4110eede090ab436248fee6c
MajedFakhrEldin <majed.ctb@gmail.com>
2022-04-28 01:12:37 +0200

    Added optional chaining in case there are no locations

--- src/components/DashboardLeft.jsx
+++ src/components/DashboardLeft.jsx
@@ -5,7 +5,7 @@ import { Grid } from '@mui/material';
 const Left = ({locations, selectedLocation}) => {
     return (
       <div>        
-        {locations.map((location, index) =>(
+        {locations?.map((location, index) =>(
           <div key={location.id} style={{margin: "5px 0"}}>
             <LocationRow key={locations.id} locName={location.name} selected={index === selectedLocation}/>
           </div>
