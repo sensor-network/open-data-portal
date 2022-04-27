47d003f05c2edfe9df2ce256cab736d246538d1e
nilsperssonsuorra <82161471+nilsperssonsuorra@users.noreply.github.com>
2022-04-27 20:44:10 +0200

    Update AdminDashboard.jsx
    * Added fetch for database and server health.
    * Added message for sensor if database is down.

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -9,12 +9,38 @@ const healthData = [
   {
     name: 'Sensors',
     status: 1.0,
-    lastCheckTime: 1611990000000,
+    lastCheckTime: 0,
+    datapoints: [],
+    elements: []
+  },
+  {
+    name: 'Server',
+    status: 1.0,
+    lastCheckTime: 0,
     datapoints: [
 
     ],
     elements: [
-
+      {
+        name: 'Server',
+        status: 1.0,
+        lastCheckTime: 0,
+        datapoints: []
+      }
+    ]
+  },
+  {
+    name: 'Database',
+    status: 1.0,
+    lastCheckTime: 0,
+    datapoints: [],
+    elements: [
+      {
+        name: 'Database',
+        status: 1.0,
+        lastCheckTime: 0,
+        datapoints: []
+      }
     ]
   }
 ]
@@ -23,8 +49,9 @@ const healthData = [
 
 const emptylist = []
 
-async function fetchSensordata() {
-  const response = await fetch('/api/v3/sensors/status');
+// Fetches the data from health/sensors and puts the data in healthData
+async function fetchSensorData() {
+  const response = await fetch('/api/v3/health/sensors');
   // waits until the request completes...
   const sensorData = await response.json();
 
@@ -39,30 +66,54 @@ async function fetchSensordata() {
       emptylist.push({"name":sensorData[i].name, "status": 0.0, lastCheckTime: sensorData[i].lastActive, datapoints: []})
       healthData[0].lastCheckTime = sensorData[i].lastActive
     }
-
-    
   }
 
   for (var i = 0; i < emptylist.length; i++) {
     healthData[0].elements?.push(emptylist[i])}
 
   for (let i = 0; i < healthData[0].elements.length; i++) {
-    console.log(healthData[0].elements[i].status)
     if (healthData[0].elements[i].status === 0)
     {
       healthData[0].status = 0.5
     }
   }
-  //for (var i = 0; i < emptylist.length; i++) {
-  //  healthData[0].datapoints?.push(emptylist[i])}
-  console.log("logging")
-  console.log(healthData)
-
 }
 
+// Fetches the data from health/status and puts the data in healthData
+async function fetchStatusData() {
+  const response = await fetch('/api/v3/health/status');
+  // waits until the request completes...
+  const StatusData = await response.json();
+  var today = new Date();
+  healthData[1].lastCheckTime = today
+  healthData[2].lastCheckTime = today
+  healthData[1].elements[0].lastCheckTime = today
+  healthData[2].elements[0].lastCheckTime = today
 
-fetchSensordata()
+  if (StatusData.status.server == "OK") {
+    healthData[1].status = 1.0
+    healthData[1].elements[0].status = 1.0
+    
+  }
+  else {
+    healthData[1].status = 0.0
+    healthData[1].elements[0].status = 0.0
+  }
+  if (StatusData.status.database == "OK") {
+    healthData[2].status = 1.0
+    healthData[2].elements[0].status = 1.0
+  }
+  else {
+    healthData[2].status = 0.0
+    healthData[2].elements[0].status = 0.0
+    healthData[0].status = null
+    healthData[0].elements = [{"name":"No sensor data avaliable, database is down.", datapoints: []}]
 
+  }
+}
+
+fetchSensorData()
+fetchStatusData()
 
 
 const AdminView = () => {
