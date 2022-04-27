78e8e832d020a4a28139c5f61c509841e905fcff
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-27 19:55:36 +0200

    naming changes

--- src/lib/database/connection.ts
+++ src/lib/database/connection.ts
@@ -1,34 +1,34 @@
-import { createPool, Pool } from 'mysql2/promise';
-
+import { createPool, Pool } from "mysql2/promise";
 
 let pool: Pool | undefined = undefined;
 
 export async function getConnectionPool(): Promise<Pool> {
-    /* returns a mysql connection pool */
-    if (pool) {
-        /* pool is created, return existing one */
-        return pool;
-    }
-
-    pool = await createPool({
-        host: process.env.NEXT_PUBLIC_DB_HOST,
-        user: process.env.NEXT_PUBLIC_DB_USER,
-        password: process.env.NEXT_PUBLIC_DB_PASSWORD,
-        database: process.env.NEXT_PUBLIC_DB_DATABASE,
-        timezone: "+00:00",
-        connectionLimit: 100,
-        // ssl      : {"rejectUnauthorized":true},
-    });
-
+  /* returns a mysql connection pool */
+  if (pool) {
+    /* pool is created, return existing one */
     return pool;
+  }
+
+  pool = await createPool({
+    host: process.env.NEXT_PUBLIC_DB_HOST,
+    user: process.env.NEXT_PUBLIC_DB_USER,
+    password: process.env.NEXT_PUBLIC_DB_PASSWORD,
+    database: process.env.NEXT_PUBLIC_DB_DATABASE,
+    timezone: "+00:00",
+    connectionLimit: 100,
+    // ssl      : {"rejectUnauthorized":true},
+  });
+
+  return pool;
 }
 
-export async function endConnection() : Promise<void> {
-    /* closes down the pool connection if one exists */
-    if (pool) await pool.end();
+export async function endConnection(): Promise<void> {
+  /* closes down the pool connection if one exists */
+  if (pool) await pool.end();
 }
 
-export const status = async ({}: {}) => {
-    const connection = await getConnectionPool();
-    const result = await connection.query(`DESCRIBE station;`);
-  };
+export const getStatus = async () => {
+  /* this is just a simple query to see if the db is running and the connection is working */
+  const connection = await getConnectionPool();
+  await connection.query(`DESCRIBE station;`);
+};
