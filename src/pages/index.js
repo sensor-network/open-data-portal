7217a15234c7355282d9f6bf0922c7b412940545
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-28 09:54:35 +0200

    added container around the dashboard

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,33 @@
 import Head from "next/head";
+import Container from "@mui/material/Container";
 import Welcome from "/src/components/Welcome";
-import Dashboard from "src/components/Dashboard"
+import Dashboard from "src/components/Dashboard";
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Open Data Portal</title>
-        <meta name="description" content="Data portal for Karlskrona Archipelago Water Quality"/>
-        <link rel="icon" href="/favicon.ico"/>
+        <meta
+          name="description"
+          content="Data portal for Karlskrona Archipelago Water Quality"
+        />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        {/*<Welcome/>*/}
-        <Dashboard/>
+        <Container
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 50,
+            marginBottom: 50,
+          }}
+          maxWidth={"xxl"}
+        >
+          {/*<Welcome/>*/}
+          <Dashboard />
+        </Container>
       </main>
     </div>
   );
 }
-
