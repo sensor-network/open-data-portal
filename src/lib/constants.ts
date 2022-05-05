11d2bf3389fabea7837afad82cb1ed58079dd5b9
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-05-05 21:40:26 +0200

    refactor status contants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,19 @@
 export const DECIMAL_PLACES = 2;
 
 export const SRID = 4326;
+
+export const HTTP_STATUS = {
+  /* 2xx Success */
+  OK: 200,
+  CREATED: 201,
+
+  /* 4xx Client Error */
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  NOT_ALLOWED: 405,
+
+  /* 5xx Server Error */
+  SERVER_ERROR: 500,
+};
