b98bd955aee474c901e53b46914f5d64873dd33e
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-05-11 14:04:33 +0200

    dont select unselectable indices

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,11 +1,14 @@
-import { useEffect, useState, useContext, useCallback, useMemo } from "react";
+import { useState, useContext, useMemo } from "react";
 import type { PointTuple } from "leaflet";
 import type { Location } from "~/lib/database/location";
 import { PreferenceContext } from "~/lib/utils/preferences";
+import { useInterval } from "~/lib/hooks";
 import LocationRow from "./LocationRow";
 import Card from "./Card";
-
 import styles from "src/styles/Dashboard.module.css";
+
+const MAP_CENTER: PointTuple = [56.178516, 15.60261];
+const INITIAL_DELAY = 5000;
 
 /* load map without ssr due to lack of support with Leaflet */
 import dynamic from "next/dynamic";
@@ -17,17 +20,11 @@ const MapWithNoSSR = dynamic(() => import("./DashboardMap"), {
   mapCenter: PointTuple;
 }>;
 
-const mapCenter: PointTuple = [56.178516, 15.60261];
-
 const Dashboard: React.FC = () => {
   const { locations } = useContext(PreferenceContext);
-  const TIMEOUT_MS = 5000;
 
+  const [intervalDelay, setIntervalDelay] = useState(INITIAL_DELAY);
   const [selectedLocationIndex, setSelectedIndex] = useState(0);
-  const maxSelectableIndex = useMemo(() => {
-    if (!locations) return 0;
-    return locations.length - 1;
-  }, [locations]);
 
   const [unselectableIndices, setUnselectableIndices] = useState<number[]>([]);
   const dontSelectThis = (index: number) => {
@@ -40,32 +37,22 @@ const Dashboard: React.FC = () => {
     setUnselectableIndices((prev) => prev.filter((i) => i !== index));
   };
 
-  const getNextIdx: (currentIdx: number) => number = useCallback(
-    (currentIdx) => {
-      // get next idx with wrap around, dont select unselectable locations
-      const nextIdx = currentIdx === maxSelectableIndex ? 0 : currentIdx + 1;
-      // recursively call until we get a valid idx
-      if (!unselectableIndices.includes(nextIdx)) {
-        return nextIdx;
-      }
-      return getNextIdx(nextIdx);
-    },
-    [unselectableIndices, maxSelectableIndex]
-  );
-
   const selectNextLocation = () => {
-    setSelectedIndex((prev) => getNextIdx(prev));
-    setTimeout(selectNextLocation, TIMEOUT_MS);
+    if (!locations) return;
+    const getNextId: (arg0: number) => number = (currentId: number) => {
+      // get next idx with wrap around, dont select unselectable locations
+      const nextId = currentId === locations.length - 1 ? 0 : currentId + 1;
+      // recursively call until we get a valid idx
+      if (!unselectableIndices.includes(nextId)) {
+        return nextId;
+      }
+      return getNextId(nextId);
+    };
+
+    setSelectedIndex((prev) => getNextId(prev));
   };
 
-  useEffect(() => {
-    /* initialize a timeout when locations arrive */
-    if (locations) {
-      setTimeout(selectNextLocation, TIMEOUT_MS);
-    }
-  }, [locations]);
-
-  useEffect(() => console.log(unselectableIndices));
+  useInterval(selectNextLocation, intervalDelay);
 
   return (
     <Card title="Dashboard" styles={{ margin: "40px 0 0 0" }}>
@@ -91,7 +78,7 @@ const Dashboard: React.FC = () => {
               <MapWithNoSSR
                 locations={locations}
                 selectedLocation={selectedLocationIndex}
-                mapCenter={mapCenter}
+                mapCenter={MAP_CENTER}
               />
             </div>
           </>
