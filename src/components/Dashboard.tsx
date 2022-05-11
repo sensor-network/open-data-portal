6efb94497c244744a3e164ab8b339a40424038f4
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-05-11 15:41:20 +0200

    include slider to select interval-speed

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useMemo } from "react";
+import { useState, useContext, useReducer } from "react";
 import type { PointTuple } from "leaflet";
 import type { Location } from "~/lib/database/location";
 import { PreferenceContext } from "~/lib/utils/preferences";
@@ -6,6 +6,12 @@ import { useInterval } from "~/lib/hooks";
 import LocationRow from "./LocationRow";
 import Card from "./Card";
 import styles from "src/styles/Dashboard.module.css";
+
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./CustomProgressBar";
+import { Stack, Slider } from "@mui/material";
+import SlowMotionVideoIcon from "@mui/icons-material/SlowMotionVideo";
+import SpeedIcon from "@mui/icons-material/Speed";
 
 const MAP_CENTER: PointTuple = [56.178516, 15.60261];
 const INITIAL_DELAY = 5000;
@@ -20,22 +26,32 @@ const MapWithNoSSR = dynamic(() => import("./DashboardMap"), {
   mapCenter: PointTuple;
 }>;
 
+/** reducer for a state with unique array elements, only two actions
+ * allowed - push and pop. push only adds an element if it is not already
+ * in the array, pop only removes an element if it is in the array
+ **/
+export const uniqueArrayReducer = (
+  state: number[],
+  action: { type: "push" | "pop"; index: number }
+) => {
+  switch (action.type) {
+    case "push":
+      if (state.includes(action.index)) return state;
+      return [...state, action.index];
+    case "pop":
+      return state.filter((i) => i !== action.index);
+  }
+};
+
 const Dashboard: React.FC = () => {
   const { locations } = useContext(PreferenceContext);
 
   const [intervalDelay, setIntervalDelay] = useState(INITIAL_DELAY);
   const [selectedLocationIndex, setSelectedIndex] = useState(0);
-
-  const [unselectableIndices, setUnselectableIndices] = useState<number[]>([]);
-  const dontSelectThis = (index: number) => {
-    // only add if not already in
-    if (unselectableIndices.includes(index)) return;
-    setUnselectableIndices((prev) => [...prev, index]);
-  };
-  const canSelectThis = (index: number) => {
-    // remove index from unselectable
-    setUnselectableIndices((prev) => prev.filter((i) => i !== index));
-  };
+  const [unselectableIndices, updateUnselectable] = useReducer(
+    uniqueArrayReducer,
+    []
+  );
 
   const selectNextLocation = () => {
     if (!locations) return;
@@ -56,6 +72,23 @@ const Dashboard: React.FC = () => {
 
   return (
     <Card title="Dashboard" styles={{ margin: "40px 0 0 0" }}>
+      <Stack direction="row" alignItems="center" spacing={2} sx={{ my: 2 }}>
+        <ThemeProvider theme={theme}>
+          <SlowMotionVideoIcon color="primary" fontSize="large" />
+          <Slider
+            aria-label="Interval Speed"
+            valueLabelDisplay="auto"
+            valueLabelFormat={(v: number) => `${Math.round(v / 1000)}s`}
+            value={intervalDelay}
+            onChange={(e, v) => setIntervalDelay(v as number)}
+            min={1 * 1000} // 1s
+            max={60 * 1000} // 1min
+            color="primary"
+          />
+          <SpeedIcon color="primary" fontSize="large" />
+        </ThemeProvider>
+      </Stack>
+
       <div className={styles.container}>
         {locations ? (
           <>
@@ -67,8 +100,12 @@ const Dashboard: React.FC = () => {
                       key={location.id}
                       locationName={location.name}
                       selected={index === selectedLocationIndex}
-                      dontSelectThisOne={() => dontSelectThis(index)}
-                      canSelectThisOne={() => canSelectThis(index)}
+                      dontSelectThisOne={() =>
+                        updateUnselectable({ type: "push", index })
+                      }
+                      canSelectThisOne={() =>
+                        updateUnselectable({ type: "pop", index })
+                      }
                     />
                   </div>
                 ))}
