5ae7fbc34fbadac4ecaaba6c9735ed8bee442299
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-05-13 12:39:39 +0200

    add footer information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,39 @@
 import Container from "@mui/material/Container";
 import { PRIMARY_BLUE_COLOR } from "~/lib/constants";
+import styles from "~/styles/Footer.module.css";
+import Link from "next/link";
 
 const Footer: React.FC = () => {
   return (
-    <div
-      style={{
-        borderTop: `1px solid ${PRIMARY_BLUE_COLOR}`,
-        boxShadow: `0 0 5px 0 ${PRIMARY_BLUE_COLOR}`,
-      }}
-    >
-      <Container style={{ padding: 20 }} maxWidth="xl">
-        <p>{"<insert footer stuff here>"}</p>
-      </Container>
+    <div className={styles.footer}>
+      <p>
+        Copyright (c) 2022 under the{" "}
+        <Link href="https://github.com/sensor-network/open-data-portal/blob/main/LICENSES/MIT.txt">
+          MIT License.
+        </Link>{" "}
+        Made by students at <Link href="https://bth.se">BTH</Link>:
+      </p>
+      <ul className={styles.students}>
+        <li>
+          <Link href="https://github.com/juliusmarminge">Julius Marminge</Link>
+        </li>
+        <li>
+          <Link href="https://github.com/MajedFakhrEldin">
+            Majed Fakhr Eldin
+          </Link>
+        </li>
+        <li>
+          <Link href="https://github.com/nilsperssonsuorra">
+            Nils Persson Suorra
+          </Link>
+        </li>
+        <li>
+          <Link href="https://github.com/anva19">Andre Varga</Link>
+        </li>
+        <li>
+          <Link href="https://github.com/Peshkatar">Arlind Iseni</Link>
+        </li>
+      </ul>
     </div>
   );
 };
