e18b3ef24e18d8fe4c082a7ec876757903631c5a
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-05-19 13:47:17 +0200

    bug: remove time range validation

--- src/lib/validators/time.ts
+++ src/lib/validators/time.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { isValid, endOfToday } from "date-fns";
+import { isValid, endOfToday, formatISO } from "date-fns";
 
 export const zTime = z
   .string()
@@ -14,18 +14,21 @@ export const zTimeRange = z
   .object({
     startTime: z
       .string()
-      .default("2022Z" /* new year 2022 */)
+      .default("2022-01-01T00:00:00Z" /* new year 2022 */)
       .refine((str) => isValid(new Date(str)), "Unable to parse string as Date")
+      /** FIXME: The range should probably be validated in the future. Right now there are some 
+       * time-sync issues with the backend and the frontend causing these validations to be flaky.
       .refine((str) => new Date(str) >= new Date("2022Z"), "must be after 2022")
+      */
       .transform((str) => new Date(str)),
     endTime: z
       .string()
-      .default(new Date().toISOString() /* current time */)
+      .default(formatISO(new Date()) /* current time */)
       .refine((str) => isValid(new Date(str)), "Unable to parse string as Date")
       /** FIXME: Should not accept future dates, but there seems to be some time difference 
        * between the client and the server which causes the time to be out-of-sync.
       .refine((str) => new Date(str) <= endOfToday(), "can't be a future date") 
-     */
+      */
       .transform((str) => new Date(str)),
   })
   .refine(
